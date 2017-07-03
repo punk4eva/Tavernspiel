4155b32038ab2676938a02b059050933b7997a7c
Adam <Adam@Home.home>
2017-07-03 20:16:40 +0100

    Update

--- Tavernspiel/src/gui/MainClass.java
+++ Tavernspiel/src/gui/MainClass.java
@@ -1,10 +1,22 @@
 
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  *
  * @author Adam Whittaker
  */
-public class MainClass{
+public class MainClass implements ActionListener{
+    
+    public MainClass(){
+    
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
