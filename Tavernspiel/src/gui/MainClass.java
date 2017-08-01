b36178f0b79ee460fba230e7ad063244988aa30c
Adam <Adam@Home.home>
2017-08-01 15:25:56 +0100

    Added new tileset!!! (still work in progress)

--- Tavernspiel/src/gui/MainClass.java
+++ Tavernspiel/src/gui/MainClass.java
@@ -50,7 +50,7 @@ public class MainClass extends Canvas implements ActionListener, Runnable{
     public static final IDHandler idhandler = new IDHandler(); //Creates UUIDs for GameObjects.
     public static final GrimReaper reaper = new GrimReaper(); //Handles death.
     public static final BuffEventInitiator buffinitiator = new BuffEventInitiator(); //Handles buffs.
-    public static Area area1 = RoomBuilder.floodedVault(new Location("Test", "temporaryTiles"), ItemBuilder.amulet());
+    public static Area area1 = RoomBuilder.standard(new Location("Test", "shkoderTileset"));
     public static long frameNumber = 0;
     private int framerate = 0;
 
@@ -64,10 +64,7 @@ public class MainClass extends Canvas implements ActionListener, Runnable{
     }
 
     public static void main(String[] args){
-        ReadWrite rw = new ReadWrite("filetesting/test.txt");
-        rw.write("TEST STRING");
-        System.out.println(rw.read());
-        //MainClass mc = new MainClass();
+        MainClass mc = new MainClass();
     }
 
     @Override
@@ -118,6 +115,7 @@ public class MainClass extends Canvas implements ActionListener, Runnable{
         g.setColor(Color.black);
         g.fillRect(0, 0, WIDTH, HEIGHT);
         handler.render(g);
+        paintArea(area1, g);
         framerate++;
         if(framerate == 60){
             frameNumber = (frameNumber+1) % 1000000000;
