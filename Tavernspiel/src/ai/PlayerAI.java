e1c4b9d1f2e4a48d5ff23efcc42bb1583df86739
WHSB Student <WHSB Student@whsbLaptop2.local>
2017-09-26 12:52:14 +0100

    edit

--- Tavernspiel/src/ai/PlayerAI.java
+++ Tavernspiel/src/ai/PlayerAI.java
@@ -1,12 +1,35 @@
 
 package ai;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 /**
  *
  * @author Adam Whittaker
  * 
  * The AI of the Hero.
  */
-public class PlayerAI extends AITemplate{
+public class PlayerAI extends AITemplate implements KeyListener{
+
+    @Override
+    public void keyTyped(KeyEvent e){
+        switch(e.getKeyChar()){
+            case 'w':
+            case 'a':
+            case 's':
+            case 'd':
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
