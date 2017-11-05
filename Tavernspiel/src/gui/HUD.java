6537b5124173474799fffe3fa004b8af428dece2
emerz35 <charlie.h.hands@gmail.com>
2017-11-05 15:59:21 +0000

    updated HUD

--- Tavernspiel/src/gui/HUD.java
+++ Tavernspiel/src/gui/HUD.java
@@ -17,10 +17,11 @@ public class HUD implements Viewable, ScreenListener{
     private final List<Screen> screens = new LinkedList<>();
     {
         screens.add(new Screen("Player",5,5,60,60,this));
-        screens.add(new Screen("Wait",0,Game.HEIGHT - 20,20,20,this));
+        screens.add(new Screen("Wait",Game.WIDTH - 50, Game.HEIGHT - 73, 40, 40,this));
+        screens.add(new Screen("Search",Game.WIDTH - 95, Game.HEIGHT - 73, 40, 40,this));
+        screens.add(new Screen("Inventory",Game.WIDTH - 140, Game.HEIGHT - 73, 40, 40,this));
     }
     private final MainClass main;
-    
     
     public HUD(MainClass m){
         main = m;
@@ -36,16 +37,23 @@ public class HUD implements Viewable, ScreenListener{
     public void paint(Graphics g){
         g.setColor(ConstantFields.backColor);
         g.fillRect(5, 5, 60, 60);
-        g.drawImage(main.player.animator.active.frames[0].getImage().getScaledInstance(60, 60, 0),5,0,null);
+        g.drawImage(main.player.animator.active.frames[0].getImage().getScaledInstance(60, 60, 0),5,2,null);
         g.fillRect(70,5,200,10);
+        
+        g.fill3DRect(Game.WIDTH - 50, Game.HEIGHT - 73, 40, 40, true);
+        g.fill3DRect(Game.WIDTH - 95, Game.HEIGHT - 73, 40, 40, true);
+        g.fill3DRect(Game.WIDTH - 140, Game.HEIGHT - 73, 40, 40, true);
+        
         g.setColor(Color.red);
         g.fill3DRect(70, 5, (int) ((double)main.player.attributes.hp/(double)(main.player.attributes.maxhp) * 200), 10, true);
-        g.fill3DRect(0, Game.HEIGHT - 20, 20, 20, true);
+       
+
     }
 
     @Override
     public void screenClicked(ScreenEvent name){
-        throw new UnsupportedOperationException("Not supported yet.");
+        switch(name.getName()){
+        }
     }
     
 }
